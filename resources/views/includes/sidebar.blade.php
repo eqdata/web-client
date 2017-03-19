73dd9eac3761e10428464da9591b3d5290da8c34
Rocky Slavin <rocky.slavin@gmail.com>
2017-03-19 14:16:35 -0500

    added favicon

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,6 +1,6 @@
 <div class="well" style="margin-top: 40%">
     <h4>About P99Tunnel</h4>
-    <p>P99Tunnel is an <a href="https://github.com/eqdata">open source</a> project for the collection and distribution of
+    <p>P99Tunnel is an <a target="_blank" href="https://github.com/eqdata">open source</a> project for the collection and distribution of
         Project 1999 auction data. Our development team consists of a few P99 players who work on the project in their
         spare time.</p>
     </div>
