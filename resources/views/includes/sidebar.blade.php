f6e95702c0c374b5de9e605ce875bdcb194322e6
Rocky Slavin <rocky.slavin@gmail.com>
2017-04-05 12:13:26 -0500

    updated beta message in sidebar

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -3,8 +3,11 @@
     <p>P99Tunnel is an <a target="_blank" href="https://github.com/eqdata">open source</a> project for the collection and distribution of
         Project 1999 auction data. Our development team consists of a few P99 players who work on the project in their
         spare time. If you find this site useful and would like to contribute to our server costs, please feel free
-    to <a href="#" data-toggle="modal" data-target="#donate-modal">donate</a>.</p>
-    <strong>This site is currently in beta so expect bugs.</strong>
+    to <a href="#" data-toggle="modal" data-target="#donate-modal">donate</a>.<p>
+
+    </div>
+<div class="well well-warning">
+    P99Tunnel is currently in beta so expect bugs.
     </div>
     <!-- /.row -->
 </div>
