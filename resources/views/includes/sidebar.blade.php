106441772ffc92dd07547bd68ea38d308c0cb203
Rocky Slavin <rocky.slavin@gmail.com>
2017-04-05 12:18:51 -0500

    text clarification

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -2,7 +2,7 @@
     <h4>About P99Tunnel</h4>
     <p>P99Tunnel is an <a target="_blank" href="https://github.com/eqdata">open source</a> project for the collection and distribution of
         Project 1999 auction data. Our development team consists of a few P99 players who work on the project in their
-        spare time. If you find this site useful and would like to contribute to our server costs, please feel free
+        spare time. If you find this site useful and would like to help by contributing towards our server costs, please feel free
     to <a href="#" data-toggle="modal" data-target="#donate-modal">donate</a>.<p>
 
     </div>
