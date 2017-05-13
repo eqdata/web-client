ea51ed68ad90424ae652af3f14d08428cee74655
Alexander Sims <alexander.sims92@gmail.com>
2017-05-13 17:28:09 +0100

    Added ad-sense link

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -22,3 +22,11 @@
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-6934976816209542",
+            enable_page_level_ads: true
+        });
+    </script>
